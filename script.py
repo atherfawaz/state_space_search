d9b5ac79c831f047d3c50e14c36ac5c5df53f46b
atherfawaz <atherfawaz@users.noreply.github.com>
2020-02-05 17:53:49 +0500

    File reading

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
-print ('Hello World')
+#State Space Search
+
+
+#File reading
+fileobj = open("grid.txt", "r")
+if (fileobj.mode == 'r'):
+    contents = fileobj.read()
+    print(contents)
+    fileobj.close()
